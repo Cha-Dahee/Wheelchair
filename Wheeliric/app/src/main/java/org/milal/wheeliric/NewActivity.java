3a280e930946390d5054d1ebe29d0653d3a0649e
yoojung <codls7@naver.com>
2017-02-02 15:02:31 +0900

    Jsoup Parsing

--- Wheeliric/app/src/main/java/org/milal/wheeliric/NewActivity.java
+++ Wheeliric/app/src/main/java/org/milal/wheeliric/NewActivity.java
@@ -2,7 +2,6 @@ package org.milal.wheeliric;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
