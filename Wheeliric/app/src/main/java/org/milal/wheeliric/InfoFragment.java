bab508edf82dcdb58e7d3674bbc51e75d36fba22
Cha-Dahee <ekgmlekgml20@naver.com>
2017-02-06 16:42:17 +0900

    temp

--- Wheeliric/app/src/main/java/org/milal/wheeliric/InfoFragment.java
+++ Wheeliric/app/src/main/java/org/milal/wheeliric/InfoFragment.java
@@ -1,13 +1,12 @@
 package org.milal.wheeliric;
 
-
+;;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
